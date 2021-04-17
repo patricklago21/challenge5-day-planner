4ee4f5a5d4cb76f7e037a18f25e8690b61e0ae65
Patrick Lago <Patrick@Dinas-Air.attlocal.net>
2021-04-17 01:03:45 -0700

    need to test timer functionality in the morning

--- script.js
+++ script.js
@@ -10,13 +10,32 @@ $(".saveBtn").on("click", function () {
     localStorage.setItem(time, text);
 })
 
-$("#timeNine .description").val(localStorage.getItem("timeNine"));
-$("#timeTen .description").val(localStorage.getItem("timeTen"));
-$("#timeEleven .description").val(localStorage.getItem("timeEleven"));
-$("#timeTwelve .description").val(localStorage.getItem("timeTwelve"));
-$("#timeOne .description").val(localStorage.getItem("timeOne"));
-$("#timeTwo .description").val(localStorage.getItem("timeTwo"));
-$("#timeThree .description").val(localStorage.getItem("timeThree"));
-$("#timeFour .description").val(localStorage.getItem("timeFour"));
-$("#timeFive .description").val(localStorage.getItem("timeFive"));
+$("#time9 .description").val(localStorage.getItem("time9"));
+$("#time10 .description").val(localStorage.getItem("time10"));
+$("#time11 .description").val(localStorage.getItem("time11"));
+$("#time12 .description").val(localStorage.getItem("time12"));
+$("#time1 .description").val(localStorage.getItem("time1"));
+$("#time2 .description").val(localStorage.getItem("time2"));
+$("#time3 .description").val(localStorage.getItem("time3"));
+$("#time4 .description").val(localStorage.getItem("time4"));
+$("#time5 .description").val(localStorage.getItem("time5"));
 $("#timeHappyHour .description").val(localStorage.getItem("timeHappyHour"));
+
+
+function hourTimer() {
+    var currentTime = moment().hour();
+
+    $(".time-block").each(function () {
+        var eachHour = parseInt($(this).attr("id").split("hour")[1]);
+
+        if (eachHour === currentTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else if (eachHour > currentTime) {
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+
+hourTimer();
